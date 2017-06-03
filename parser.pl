9240adecb8ba99f5fa6f329f86d58a3e84f8f6b9
Jon Anderson <jon.david.anderson@gmail.com>
2017-06-03 14:00:46 -0700

    Implemented alternative configuration syntax.

---
+++ parser.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+while(<STDIN>) {
+    # Remove comments
+    s/(?<!\\)#.*\n/\n/g;
+
+    # Remove newlines not
+    # preceeded by a semi
+    # colon
+    # TODO: Count the number
+    # of replace lines and
+    # add blanks afterwards
+    # to preserve line numbers
+    # of other commands.
+    s/(?<!;)\s*\n//g;
+
+    # Remove semicolons
+    s/;\s*\n/\n/g;
+
+    print;
+}
+
