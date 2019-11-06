e4444018781187970b505b30b166f7b537c185f4
Jon Anderson <jon.david.anderson@gmail.com>
2019-11-05 23:56:04 -0800

    Saving test data.

--- tests/maker_test.go
+++ tests/maker_test.go
@@ -30,18 +30,34 @@ func TestFramework(t *testing.T) {
 	defer args.close()
 
 	mock := newMock(args.port, nil, nil)
-
 	fs := Files(mock.dir)
 	fs.file("config.mkr", `
-$(call exec, dir,
+$(call slib, d1,
 	# Dependencies
 	,
 	# Compile Flags
 	,
 	# Linking Flags
+	--kill
+);
+$(call exec, d2,
+	# Dependencies
+	d1,
+	#Compile Flags
+	-x {0}+,
+	#Linking Flags
+	--hope --love
 );`)
 
-	fs.dir("dir").file("file.cpp", "")
+	fs.dir("d1").
+		file("f1.cpp", "#include\"f3.hpp\"").
+		file("f2.cpp", "#include\"f3.hpp\"").
+		file("f3.hpp", "").
+		file("f4.hpp", "")
+
+	fs.dir("d2").
+		file("f1.cpp", "#include\"f2.hpp\"").
+		file("f2.hpp", "")
 
 	mock.Run(t)
 
